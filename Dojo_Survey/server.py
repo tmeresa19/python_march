50665df08b578505e03c6d354d83f2644551d461
Tigist Meresa <tmeresa19@gmail.com>
2023-03-15 18:50:41 -0700

    Done more updates

--- Dojo_Survey/server.py
+++ Dojo_Survey/server.py
@@ -10,49 +10,7 @@ app.secret_key = "password1234"
 
 
 
-
 @app.route('/todos', methods=['GET'])
 def get_todos():
-    return render_template("home.html", list_of_todos=list_of_todos)
+     return render_template("home.html", list_of_todos=list_of_todos)
 
-
-"""
-Method: GET
-Getting all of a particular list
-Function: get_all_todos()
-          get_todos()
-Url: '/todos'
-
-Method: GET
-Getting one item of a particular list
-Function: get_todo_by_id( id )
-          get_todo( id )
-Url: '/todo/<int:todo_id>'
-     '/todo/<int:id>'
-
-Method: GET
-Displaying a form that will eventually refer to a list
-Function: display_todo_form()
-Url: '/todo/form'
-
-Method: POST
-Create a new item of a particular list
-Function: create_todo()
-          add_todo()
-Url: '/todo/new'
-     '/todo/add'
-
-Method: POST/PUT
-Update an existing item of a particular list
-Function: update_todo( id )
-          edit_todo( id )
-Url: '/todo/update'
-     '/todo/edit'
-
-Method: POST/DELETE
-Remove an existing item from a particular list
-Function: delete_todo( id )
-          remove_todo( id )
-Url: '/todo/delete'
-     '/todo/remove'
-"""
--- ER_Diagrams/MySQL_Countries.sql
+++ ER_Diagrams/MySQL_Countries.sql
@@ -1,6 +1,51 @@
-1--
+-- 1. What query would you run to get all the countries that speak Slovene? Your query should return the name of the country, language and language percentage. Your query should arrange the result by language percentage in descending order. (1)
+
 SELECT  *
 FROM countries AS c JOIN languages AS l 
-ON c.id = l.country_id AND l.language = 'Slovene
+ON c.id = l.country_id
+WHERE l.language = 'Slovene';
 
+
+-- 2. What query would you run to display the total number of cities for each country? Your query should return the name of the country and the total number of cities. Your query should arrange the result by the number of cities in descending order. (3)
+SELECT con.name, cit.cities, COUNT(*) as Total_Number_Of_Cities
+FROM countries AS con JOIN cities AS cit
+ON con.id = cit.country_id
+GROUP BY con.name
+ORDER BY con.name DESC;
+
+-- 3. What query would you run to get all the cities in Mexico with a population of greater than 500,000? Your query should arrange the result by population in descending order. (1)
+SELECT cit.name, cit.population
+FROM countries AS con JOIN cities AS cit
+ON con.id = cit.country_id
+WHERE con.name='Mexico' and cit.population=500000;
+
+-- 4. What query would you run to get all languages in each country with a percentage greater than 89%? Your query should arrange the result by percentage in descending order. (1)
+SELECT  c.name, l.language, l.percentage
+FROM countries AS c JOIN languages AS l 
+ON c.id = l.country_id
+WHERE l.percentage> 89
+ORDER BY l.percentage DESC;
+
+-- 5. What query would you run to get all the countries with Surface Area below 501 and Population greater than 100,000? (2)
+SELECT con.name, cit.population, con.surface_area
+FROM countries AS con JOIN cities AS cit
+ON con.id = cit.country_id
+WHERE con.surface_area<501 and cit.population>100000;
+
+-- 6. What query would you run to get countries with only Constitutional Monarchy with a capital greater than 200 and a life expectancy greater than 75 years? (1)
+SELECT government_form, capital, life_expectancy
+FROM countries
+WHERE capital>200 and life_expectancy>75;
+
+-- 7. What query would you run to get all the cities of Argentina inside the Buenos Aires district and have the population greater than 500, 000? The query should return the Country Name, City Name, District and Population. (2)
+SELECT con.name, cit.name, cit.population, cit.district
+FROM countries AS con JOIN cities AS cit
+ON con.id = cit.country_id
+WHERE cit.population > 500000 and con.distict='Buenos Aires district';
+
+-- 8. What query would you run to summarize the number of countries in each region? The query should display the name of the region and the number of countries. Also, the query should arrange the result by the number of countries in descending order. (2)
+SELECT region, COUNT(*) as countries
+FROM countries
+GROUP BY region
+ORDER BY region DESC;
 
